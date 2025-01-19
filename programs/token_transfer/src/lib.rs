8b8315ebba480f9b8d436f02cd4c2993cd9482b0
StackSavvy519 <stacksavvy.dev519@gmail.com>
2025-01-19 08:52:17 +0000

    updated new

--- programs/token_transfer/src/lib.rs
+++ programs/token_transfer/src/lib.rs
@@ -1,15 +1,33 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, Transfer};
 
-declare_id!("5a4U1aJy23hnXWG4adUJK4Y6t7tvp3XiCTDg7Eoj6ijS");
+declare_id!("1skh4xsCzeWV73h49vuWkqPtDZE4oa7JFMLXiwg16yM");
 
 #[program]
 pub mod token_transfer {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn transfer_tokens(ctx: Context<TransferAccounts>, amount: u64) -> Result<()> {
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.from.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::transfer(cpi_ctx, amount)?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct TransferAccounts<'info> {
+    /// CHECK: safe
+    #[account(mut)]
+    pub from: AccountInfo<'info>,
+    /// CHECK: safe
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    pub authority: Signer<'info>,
+    pub token_program: Program<'info, Token>
+}
